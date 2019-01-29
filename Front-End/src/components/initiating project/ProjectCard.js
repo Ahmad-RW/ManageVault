2ed3beb1b87566ff8c6ccb302cfdc56780efaf5a
Saud Otaibi <entertainerk@gmail.com>
2019-01-29 21:28:04 +0300

    Basic request delete

--- Front-End/src/components/initiating project/ProjectCard.js
+++ Front-End/src/components/initiating project/ProjectCard.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { connect } from 'react-redux'
+import RequestDelete from './requestDelete';
+
 const ProjectCard = (props) => {
     const projects = props.projects
     const projectsList = projects.length ? (
@@ -18,6 +20,7 @@ const ProjectCard = (props) => {
                         <h4 className="card-title">{project.title}</h4>
                         <p className="card-text"> velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                         <Link to="#" className="card-link">Open Project</Link>
+                        <RequestDelete project={project}/>
                     </div>
                 </div>
             )
--- Front-End/src/store/reducers/rootReducer.js
+++ Front-End/src/store/reducers/rootReducer.js
@@ -1,41 +1,56 @@
 const initState = {
-    isAuthenticated : false,
-    projects : [],
-    userInfo : {}
+    isAuthenticated: false,
+    projects: [],
+    userInfo: {}
 }
 
-const rootReducer = (state = initState, action) =>{
-    if(action.type === 'AUTHENTICATE_THE_USER'){
+const rootReducer = (state = initState, action) => {
+    if (action.type === 'AUTHENTICATE_THE_USER') {
         return state = {
             ...state,
-            isAuthenticated : true
+            isAuthenticated: true
         }
     }
-    if(action.type === 'REMOVE_AUTH'){
-        return state ={
-            ...state,
-            isAuthenticated : false
-        }
-    }
-    if(action.type === "SET_USER_INFO"){
+    if (action.type === 'REMOVE_AUTH') {
         return state = {
             ...state,
-            userInfo : action.userInfo
+            isAuthenticated: false
         }
     }
-    if(action.type ==="CREATE_PROJECT"){
+    if (action.type === "SET_USER_INFO") {
+        return state = {
+            ...state,
+            userInfo: action.userInfo
+        }
+    }
+    if (action.type === "CREATE_PROJECT") {
         let newProjects = state.projects
         newProjects.push(action.project)
         return state = {
             ...state,
-            projects : newProjects
+            projects: newProjects
         }
     }
-    if(action.type === "SET_USER_PROJECTS"){
+    if (action.type === "SET_USER_PROJECTS") {
         console.log("in reducer")
         return state = {
             ...state,
-            projects : action.projects
+            projects: action.projects
+        }
+    }
+    if (action.type === "REQUEST_DELETE") {
+        for (var i = 0; i < this.state.projects.length; i++) {
+            if (this.state.project[i] === this.props.action.project.id) {
+                const project = this.state.project[i];
+                console.log(project);
+                let updatedProjects = state.projects
+                updatedProjects.filter(project.id === this.props.action.project.id);
+                console.log(state);
+                return state = {
+                    ...state,
+                    projects: updatedProjects
+                }
+            }
         }
     }
 
