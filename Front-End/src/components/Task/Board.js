e1e34911808a685f892ef4f7127a06f55d1daf10
Saud Otaibi <entertainerk@gmail.com>
2019-02-21 11:57:06 +0300

    Just to be able to pull

--- Front-End/src/components/Task/Board.js
+++ Front-End/src/components/Task/Board.js
@@ -4,13 +4,13 @@ import DatePicker from './DatePicker'
 import CreateTask from './CreateTask'
 import ProjectSubBar from '../layout/projectSubBar';
 import TaskTableHeader from './TaskTableHeader';
-import {setProject} from '../../store/actionCreators/projectActions'
+import { setProject } from '../../store/actionCreators/projectActions'
 
 class Board extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            currentProject:localStorage.getItem("currentProject")
+            currentProject: localStorage.getItem("currentProject")
         }
         console.log(this.props.projectInContext)
         console.log(localStorage.getItem("currentProject"))
@@ -19,8 +19,8 @@ class Board extends Component {
         let taskList
         let currentProject
         this.props.projects.forEach(project => {
-            if(project._id === localStorage.getItem("currentProject")){
-                currentProject = {...project}
+            if (project._id === localStorage.getItem("currentProject")) {
+                currentProject = { ...project }
             }
         });
         console.log(currentProject)
@@ -39,7 +39,7 @@ class Board extends Component {
                     <h4>There is no tasks  yet</h4>
                 )
         }
-        else{
+        else {
             var tasks = currentProject.tasks//lvnejfbnvofebvfenbv
             console.log(currentProject)
             if (typeof tasks === "undefined") {
@@ -48,7 +48,16 @@ class Board extends Component {
             taskList = tasks.length ? (
                 tasks.map(task => {
                     return (
-                        <h4>{task.name}</h4>
+                        <div className="container-fluid-full">
+                        <table className="table table-hover table-dark">
+                        <tbody>
+                                <tr>
+                                    <td>1</td>
+                                    <td>{task.name}</td>
+                                </tr>
+                        </tbody>
+                        </table>
+                        </div>
                     )
                 })
             ) : (
@@ -76,7 +85,7 @@ const mapStateToProps = (state) => {
 const mapDispatchToProps = (dispatch) => {
     return {
         getCurrentProject: (projectId) => dispatch({ type: "GET_CURRENT_PROJECT", projectId }),
-        setProject : (project) => dispatch(setProject(project))
+        setProject: (project) => dispatch(setProject(project))
     }
 }
 export default connect(mapStateToProps, mapDispatchToProps)(Board)
