9a9feb662ac5213adf4348f760ed64b4584a5ed9
AHMAD <ahmed-rw@outlook.com>
2019-04-12 14:02:54 +0300

    finishd redirect after register

--- Front-End/src/components/accessing the system/register.js
+++ Front-End/src/components/accessing the system/register.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import fb from '../../FirebaseConfig/authConfig';
 import axios from 'axios';
 import Navbar from '../layout/Navbar'
-
+import { connect } from 'react-redux'
 class Register extends Component {
     state = {
 
@@ -19,18 +19,18 @@ class Register extends Component {
         console.log(this.state)
     }
     handleClick = (e) => {
-        this.setState({...this.state,startSpinner:true})
+        this.setState({ ...this.state, startSpinner: true })
         e.preventDefault()
         console.log(e)
         fb.auth().createUserWithEmailAndPassword(this.state.email, this.state.password).then((res) => {
             console.log(res)
             this.postToServer()
-            localStorage.setItem('token', "cookie")
+
         }).catch((exception) => {
             console.log(exception)
             this.setState({
                 warningMessage: <div className="alert alert-danger" role="alert">{exception.message} </div>,
-                startSpinner:false
+                startSpinner: false
             })
         })
 
@@ -41,14 +41,27 @@ class Register extends Component {
             password: this.state.password,
             email: this.state.email
         }).then((res) => {
-            this.props.history.push('/login')
+            this.login()
         }).catch((exception) => {
             console.log(exception)
         })
     }
+
+    login = () => {
+        fb.auth().signInWithEmailAndPassword(this.state.email, this.state.password).then((res) => {
+            console.log(res)
+            localStorage.setItem('token', this.state.email)
+            this.props.authenticate()
+            this.props.history.push('/home');//redirection
+        }).catch((exception) => {
+            console.log(exception)
+            this.setState({ ...this.state, startSpinner: false })
+            alert("wrong email or password")
+        })
+    }
     renderspinner = () => {
-        if(this.state.startSpinner===true){
-            return(
+        if (this.state.startSpinner === true) {
+            return (
                 <div class="lds-spinner"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
             )
         }
@@ -56,28 +69,36 @@ class Register extends Component {
     render() {
         return (
             <div>
-            <Navbar />
-            <div className="card container ">
-                <div className="form-group card-body" >
-                    <form onSubmit={this.handleClick}>
-                        {this.state.warningMessage}
-                        <label>Email Address</label>
-                        <input type="email" className="form-control" id="email" placeholder="Email" onChange={this.handlechange} />
-                        <label>Password</label>
-                        <input type="password" className="form-control" id="password" placeholder="Password" onChange={this.handlechange} />
-                        <div className="row">
-                            <div className="col">
-                                <label>Name</label>
-                                <input type="text" className="form-control" id="name" placeholder="Name" onChange={this.handlechange} />
+                <Navbar />
+                <div className="card container ">
+                    <div className="form-group card-body" >
+                        <form onSubmit={this.handleClick}>
+                            {this.state.warningMessage}
+                            <label>Email Address</label>
+                            <input type="email" className="form-control" id="email" placeholder="Email" onChange={this.handlechange} />
+                            <label>Password</label>
+                            <input type="password" className="form-control" id="password" placeholder="Password" onChange={this.handlechange} />
+                            <div className="row">
+                                <div className="col">
+                                    <label>Name</label>
+                                    <input type="text" className="form-control" id="name" placeholder="Name" onChange={this.handlechange} />
+                                </div>
                             </div>
-                        </div>
-                        <button className="form-control btn btn-info sign-in-button" type="submit" >Open the Vault</button>
-                    </form>
-                    {this.renderspinner()}
+                            <button className="form-control btn btn-info sign-in-button" type="submit" >Open the Vault</button>
+                        </form>
+                        {this.renderspinner()}
+                    </div>
                 </div>
-            </div>
             </div>
         )
     }
 }
-export default(Register)
+
+const mapDispatchToProps = (dispatch, ownProps) => {
+    return {
+        authenticate: () => {
+            dispatch({ type: "AUTHENTICATE_THE_USER" })
+        },
+    }
+}
+export default connect(null, mapDispatchToProps)(Register)
