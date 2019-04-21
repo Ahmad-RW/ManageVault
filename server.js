47712e5c0f7c533cd58ddc8128fd9a9c2f1a402d
AHMAD <ahmed-rw@outlook.com>
2019-04-21 14:58:56 +0300

    deployment bug sigh

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express')
 var app = express()
-const path= require('path')
+const path = require('path')
 var bodyParser = require('body-parser');
 const projectRoute = require('./routes/projectRoutes')
 const userRoute = require('./routes/userRoutes')
@@ -11,17 +11,17 @@ app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
-    
+
 });
 //Static file declaration
-app.use(express.static(path.join(__dirname+'/Front-End/build')));
+app.use(express.static(path.join(__dirname + '/Front-End/build')));
 app.use(bodyParser.json());// post request body parser
 //app.use(express.static('public'));//I think this allows all public URLs in
-console.log(__dirname+'/Front-End/build/index.html')
+console.log(__dirname + '/Front-End/build/index.html')
 
 app.get('/', (req, res) => {
-   // res.sendFile(path.join(__dirname+'/Front-End/build/index.html'));
-   res.send('effwefwefewwefefwefwefw')  
+    res.sendFile(path.join(__dirname + '/Front-End/build/index.html'));
+    res.send('effwefwefewwefefwefwefw')
 })
 app.use('/project', projectRoute);// any route starting with /project go to project route. which is exported from projectRoutes
 app.use('/user', userRoute);// same goes here
@@ -32,9 +32,9 @@ app.use('/dropbox', dbxRoute)
 
 //catch all
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname+'/Front-End/build/index.html'));
-  })
-  
+    res.sendFile(path.join(__dirname + '/Front-End/build/index.html'));
+})
+
 // //production mode
 // if(process.env.NODE_ENV === 'production') {
 //   app.use(express.static('./Front-End/build'));
