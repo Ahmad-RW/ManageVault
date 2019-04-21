566f5abfc7beae9c8baef69fcf747b42d7168cf1
AHMAD <ahmed-rw@outlook.com>
2019-04-21 14:26:19 +0300

    route midf.

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.use(express.static(path.join(__dirname, 'Front-End/build')));
 if(process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, 'Front-End/build')));
   //
-  app.get('*', (req, res) => {
+  app.get('/', (req, res) => {
     res.sendfile(path.join(__dirname = 'Front-End/build/index.html'));
   })
 }
